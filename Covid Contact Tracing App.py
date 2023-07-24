9617e1faa938eb7dbd2f060d5007380f877bb01a
Ma. Angeline Tipa <angelinetipa@gmail.com>
2023-07-24 03:57:07 -0700

    pseudocode

---
+++ Covid Contact Tracing App.py
@@ -0,0 +1,33 @@
+# import tkinter
+# window title, geometry
+# label, description for app
+# button, to add information
+
+    # label for contact details
+    # entry, for date of submission
+    # entry, for name
+    # entry, for date of birth
+    # entry, for gender
+    # entry, for phone number
+    # entry, for address
+
+    # label for health details
+    # checkbutton, for current situation
+        # if diagnosed with Coronavirus
+            # when it started
+            # have you traveled to any places when you diagnosed with coronavirus
+                # if yes state the details of your travel
+        # if showing coronavirus symptoms
+            # when it started
+            # What conditions you currently have during this time? (Check all that apply)
+            # have you traveled to any places when you diagnosed with coronavirus
+                # if yes state the details of your travel
+
+    # label for emergency contact
+    # entry, for emergency contact information
+    # button, to submit
+
+# button, to search entries
+    # entry to search names
+        # if the name in data
+            # display that its already submitted
