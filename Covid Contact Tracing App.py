79283f4a9efb4f083f382c455732c3898d12f57e
Ma. Angeline Tipa <angelinetipa@gmail.com>
2023-07-25 10:40:05 +0800

    change sytax of fonts

--- Covid Contact Tracing App.py
+++ Covid Contact Tracing App.py
@@ -7,7 +7,7 @@ window.geometry("490x250")
 window.resizable(False,False)
 
 # label, description for app
-Label(window, text = "COVID-19 Contact Tracing Form", font = ("verdana", 12, "bold")).place(x= 110, y= 5)
+Label(window, text = "COVID-19 Contact Tracing Form", font = "verdana 12 bold").place(x= 110, y= 5)
 Label(window, text = "This form will help track down people who are being diagnosed with the virus or ").place(x= 45, y= 30)
 Label(window, text = "experiencing symptoms. This can lead to timely detection and treatment, as well as").place(x= 15, y= 50)
 Label(window, text = "preventing it from spreading further.").place(x= 15, y= 70)
@@ -19,44 +19,44 @@ def openwindow2():
     window2.geometry("490x600")
     window2.resizable(False,False)
     # label for contact details
-    Label(window2, text = "Contact Details", font = ("verdana", 12, "bold")).place(x= 5, y= 5)
+    Label(window2, text = "Contact Details", font = "verdana 12 bold").place(x= 5, y= 5)
 
     # label, for name
-    Label(window2, text = "First Name", font = ("verdana", 8, "bold")).place(x= 10, y= 35)
-    Label(window2, text = "Middle Name", font = ("verdana", 8, "bold")).place(x= 170, y= 35)
-    Label(window2, text = "Last Name", font = ("verdana", 8, "bold")).place(x= 330, y= 35)
+    Label(window2, text = "First Name", font = "verdana 8 bold").place(x= 10, y= 35)
+    Label(window2, text = "Middle Name", font = "verdana 8 bold").place(x= 170, y= 35)
+    Label(window2, text = "Last Name", font = "verdana 8 bold").place(x= 330, y= 35)
     # entry, for name
     Entry(window2, width = 23).place(x= 10, y= 55)
     Entry(window2, width = 23).place(x= 170, y= 55)
     Entry(window2, width = 23).place(x= 330, y= 55)
 
     # label, for date of birth
-    Label(window2, text = "Date of Birth", font = ("verdana", 8, "bold")).place(x= 10, y= 85)
-    Label(window2, text = "(MM/DD/YYYY)", font = ("verdana", 7)).place(x= 10, y= 100)
+    Label(window2, text = "Date of Birth", font = "verdana 8 bold").place(x= 10, y= 85)
+    Label(window2, text = "(MM/DD/YYYY)", font = "verdana 7").place(x= 10, y= 100)
     # entry, for date of birth
     Entry(window2, width = 15).place(x= 10, y= 120)
     # label, for date of submission
-    Label(window2, text = "Date of Submission ", font = ("verdana", 8, "bold")).place(x= 130, y= 85)
-    Label(window2, text = "(MM/DD/YYYY)", font = ("verdana", 7)).place(x= 130, y= 100)
+    Label(window2, text = "Date of Submission ", font = "verdana 8 bold").place(x= 130, y= 85)
+    Label(window2, text = "(MM/DD/YYYY)", font = "verdana 7").place(x= 130, y= 100)
     # entry, for date of submission
     Entry(window2, width = 22).place(x= 130, y= 120)
 
     # label, for sex
-    Label(window2, text = "Sex", font = ("verdana", 8, "bold")).place(x= 290, y= 85)
+    Label(window2, text = "Sex", font = "verdana 8 bold").place(x= 290, y= 85)
     # entry, for sex
     Entry(window2, width = 5).place(x= 290, y= 120)
 
     # label, for phone number
-    Label(window2, text = "Phone Number", font = ("verdana", 8, "bold")).place(x= 350, y= 85)
+    Label(window2, text = "Phone Number", font = "verdana 8 bold").place(x= 350, y= 85)
     # entry, for phone number
     Entry(window2, width = 17).place(x= 350, y= 120)
 
     # label, for address
-    Label(window2, text = "Address", font = ("verdana", 8, "bold")).place(x= 10, y= 150)
+    Label(window2, text = "Address", font = "verdana 8 bold").place(x= 10, y= 150)
     Entry(window2, width = 77).place(x= 10, y= 170)
 
     # label for health details
-    Label(window2, text = "Contact Details", font = ("verdana", 12, "bold")).place(x= 5, y= 210)
+    Label(window2, text = "Health Details", font = "verdana 12 bold").place(x= 5, y= 210)
 
     # checkbutton, for current situation
         # if diagnosed with Coronavirus
